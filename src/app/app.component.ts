f4313bc9f9ae3b0ffb0bd03952ccad203710dd91
Raymond Loman <raymond@carpago.nl>
2017-10-26 19:15:51 +0200

    Set the service to use Promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent implements OnInit {
    }
 
    getHeroes(): void {
-	  this.heroes = this.heroService.getHeroes();
+	  this.heroService.getHeroes().then(heroes => this.heroes = heroes);
 	}
 
 	ngOnInit(): void {
