1edffe4776652e105ab90263989b16661d6a39dd
Raymond Loman <raymond@carpago.nl>
2017-10-26 19:46:07 +0200

    Renamed the app component to heroes and made it working again. And suprisingly ... it even rocks

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -6,13 +6,10 @@ import { OnInit } from '@angular/core';
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
-  providers: [HeroService],
   styleUrls: ['./heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
-
-  title = 'Tour of Heroes';
 
   heroes:Hero[] ;
   selectedHero: Hero;
