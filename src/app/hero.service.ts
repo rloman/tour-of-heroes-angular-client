ab243877982d3e91209905eaad0f9d77b38f748e
Raymond Loman <raymond@carpago.nl>
2017-10-30 08:23:28 +0100

    Add save in component which invoked the new update() method in hero.service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,6 +10,8 @@ export class HeroService {
 
 	private heroesUrl = 'api/heroes';  // URL to web api
 
+	private headers = new Headers({'Content-Type': 'application/json'});
+
 	constructor(
 		private http: Http
 
@@ -46,4 +48,13 @@ export class HeroService {
 		  .then(response => response.json() as Hero)
 		  .catch(this.handleError);
 	}
+
+	update(hero: Hero): Promise<Hero> {
+		const url = `${this.heroesUrl}/${hero.id}`;
+		return this.http
+		  .put(url, JSON.stringify(hero), {headers: this.headers})
+		  .toPromise()
+		  .then(() => hero)
+		  .catch(this.handleError);
+	  }
 }
