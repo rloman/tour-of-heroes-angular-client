663756b7b96c933d2abad6d4269a4ba0a5044144
Raymond Loman <raymond@carpago.nl>
2017-11-02 15:32:09 +0100

    Amend login component to stefan his example on the url

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from './authentication.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    moduleId: module.id,
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+    model: any = {};
+    loading = false;
+    error = '';
 
-  constructor() { }
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        // reset login status
+        this.authenticationService.logout();
+    }
 
+    login() {
+        this.loading = true;
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(result => {
+                if (result === true) {
+                    // login successful
+                    this.router.navigate(['home']);
+                } else {
+                    // login failed
+                    this.error = 'Username or password is incorrect';
+                    this.loading = false;
+                }
+            }, error => {
+              this.loading = false;
+              this.error = error;
+            });
+    }
 }
